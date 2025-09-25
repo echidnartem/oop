3ba81caf291bd81a2de90c06cdf266c763041960
monarch <echidnartem@gmail.com>
2025-09-25 10:10:28 +0300

    Update anti_troll.cpp

--- 1/source/anti_troll.cpp
+++ 1/source/anti_troll.cpp
@@ -5,7 +5,7 @@ std::string anti_troll(std::string input_string) {
     for (char c : input_string) {
         char temp_c = c;
         c = std::tolower(c);
-        if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+        if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' && c != 'y') {
             output_string += temp_c;
         }
     }
